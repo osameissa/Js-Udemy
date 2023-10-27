04f1cf7d3d739b3c81329492890d0cd6f6473d1e
osameissa <113057277+osameissa@users.noreply.github.com>
2023-10-27 15:46:08 +0200

    Challenge #2

--- challenge2.js
+++ challenge2.js
@@ -2,8 +2,8 @@ const massMark = 78;
 const heightMark = 1.69;
 const massJohn = 92;
 const heightJohn = 1.95;
-const BMIMark = massMark / (heightMark * heightMark);
-const BMIJohn = massJohn / (heightJohn * heightJohn);
+const BMIMark = massMark / (heightMark ** 2);
+const BMIJohn = massJohn / (heightJohn ** 2);
 const higherBMI = BMIMark > BMIJohn;
 console.log(BMIMark, BMIJohn);
 
