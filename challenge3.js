04d13f2c1de71f6088c4d5632472826492562df1
osameissa <113057277+osameissa@users.noreply.github.com>
2023-10-28 14:21:25 +0200

    Challenge #3

---
+++ challenge3.js
@@ -0,0 +1,14 @@
+/*
+There are two gymnastics teams, Dolphins and Koalas. They compete against each other 3 times. The winner with the highest average score wins a trophy!
+Your tasks:
+1. Calculatetheaveragescoreforeachteam,usingthetestdatabelow
+2. Comparetheteam'saveragescorestodeterminethewinnerofthecompetition,
+and print it to the console. Don't forget that there can be a draw, so test for that
+as well (draw means they have the same average score)
+3. Bonus1:Includearequirementforaminimumscoreof100.Withthisrule,a
+team only wins if it has a higher score than the other team, and the same time a score of at least 100 points. Hint: Use a logical operator to test for minimum score, as well as multiple else-if blocks 😉
+4. Bonus2:Minimumscorealsoappliestoadraw!Soadrawonlyhappenswhen both teams have the same score and both have a score greater or equal 100 points. Otherwise, no team wins the trophy
+Test data:
+§ Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+§ Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123 § Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+*/
