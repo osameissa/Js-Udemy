9fb67740b9cdcabd4f2948a42b803bc59cb9af66
osameissa <113057277+osameissa@users.noreply.github.com>
2023-10-29 15:41:37 +0100

    switch

---
+++ lessons/switch.js
@@ -0,0 +1,39 @@
+const day = "friday";
+
+switch(day) {
+    case "monday":
+        console.log("Plan course structure");
+        console.log("Go to coding meetup");
+            break;
+        case "tuesday":
+            console.log("Prepare theory videos");
+            break;
+        case "wedneseday":
+        case "thursday":
+            console.log("Write code examples");
+            break;
+        case "friday":
+            console.log("Record videos");
+            break;
+        case "saturday":
+        case "sunday": 
+            console.log("Enjoy your weekend");
+            break;
+        default:
+            console.log("Not a valid day");
+}
+
+if (day === "monday") {
+    console.log("Plan course structure");
+    console.log("Go to coding meetup");
+} else if (day === "tuesday") {
+    console.log("Prepare theory videos");
+} else if (day === "wednesday" || day === "thursday") {
+    console.log("Write code examples");
+} else if (day === "friday") {
+    console.log("Record videos");
+} else if (day === "saturday" || day === "sunday") {
+    console.log("Enjoy your weekend");
+} else {
+    console.log("Not a valid day");
+}
