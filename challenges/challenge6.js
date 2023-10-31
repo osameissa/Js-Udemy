8ca86bcf2b3cb27702ca6dce4ceacde164f8cee5
osameissa <113057277+osameissa@users.noreply.github.com>
2023-10-31 17:30:29 +0100

    Challenge #6

--- challenges/challenge6.js
+++ challenges/challenge6.js
@@ -4,7 +4,7 @@ const calcTip = function(bill) {
 }
 
 const bills = [125, 550, 44];
-const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2]),];
-const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2], ]
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]]
 
 console.log(`Bills: ${bills}, Tips: ${tips}, Totals: ${totals}`);
