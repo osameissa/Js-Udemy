12e6bf33fa585e37fa4c3aad79c3c8c28cf783d4
osameissa <113057277+osameissa@users.noreply.github.com>
2023-11-05 00:54:43 +0100

    objectMethods

--- lessons/objectMethods.js
+++ lessons/objectMethods.js
@@ -1,21 +1,27 @@
 const bob = {
     firstName: 'Bob',
     lastName: 'Anderson',
-    birthYear: 2000,
+    birthYear: 1954,
     job: 'teacher',
-    friends: ['Michael, Steven,Peter'],
+    friends: ['Michael', 'Steven', 'Peter'],
     hasDriversLicense: true,
-    
+    currentYear: 2042,
+
     /* object method, function is a property of object (an expression)
     calcAge: function(birthYear) {
         return 2042 - birthYear;
-    } */
+    } 
 
     calcAge: function () {
         console.log(this);
-        return 2042 - this.birthYear;
+        return this.currentYear - this.birthYear;
+    } */
+
+    calcAge: function () {
+        
     }
 };
 
 console.log(bob.calcAge());
-console.log(bob['calcAge']());
+console.log(bob.calcAge());
+console.log(bob.calcAge());
