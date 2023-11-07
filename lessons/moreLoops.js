4a1f00b61a9b55ae53157bcd930a2d3d51435dd8
osameissa <113057277+osameissa@users.noreply.github.com>
2023-11-07 18:07:27 +0100

    moreLoops

--- lessons/moreLoops.js
+++ lessons/moreLoops.js
@@ -7,12 +7,12 @@ const bob = [
 ];
 
 // looping bob[] backwards
-for(let i = bob.length - 1; i >= 0; i-- ) {
+for (let i = bob.length - 1; i >= 0; i-- ) {
     console.log(i, bob[i]);
 }
 
 // loopception, loop inside loop
-for(let exercise = 1; exercise < 4; exercise++) {
+for (let exercise = 1; exercise < 4; exercise++) {
     console.log(`Starting exercise ${exercise} 🏋️!`);
 
     for(let rep = 1; rep < 11; rep++) {
@@ -20,4 +20,20 @@ for(let exercise = 1; exercise < 4; exercise++) {
     }
 }
 
-// while loop
+// while loop, only needs one condition and is optimal when there counter-variable is unknown
+let rep = 1;
+
+while (rep <= 10) {
+    console.log(`Rep ${rep}: Weight lifting 🏋️`);
+    rep++;
+}
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+console.log(`Dice: ${dice}`);
+
+// loops ends when dice = 6
+while (dice !== 6) {
+    console.log(`You rolled a: ${dice}`);
+    dice = Math.trunc(Math.random() * 6) + 1;
+    if(dice === 6) console.log('Loop is about to end...');
+}
