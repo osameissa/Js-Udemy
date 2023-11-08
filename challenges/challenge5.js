cd30bfb843eda9653a73d459a1abc7c6740c73df
osameissa <113057277+osameissa@users.noreply.github.com>
2023-11-08 15:51:10 +0100

    Challenge #5

--- challenges/challenge5.js
+++ challenges/challenge5.js
@@ -5,20 +5,20 @@ let scoreKoalas = calcAverage(65, 54, 49);
 console.log(`Average Dolphins score: ${scoreDolphins}
 Average Koalas score: ${scoreKoalas}`);
 
-const checkWinner = function(avgDolphins, avgKoalas) {
-    if (avgDolphins >= 2 * avgKoalas) {
-        console.log(`Dolphins win! (${avgDolphins} vs. ${avgKoalas})`);
-    } else if (avgKoalas >= 2 * avgDolphins) {
-        console.log(`Koalas win! (${avgKoalas} vs. ${avgDolphins})`);
-    } else {
-        console.log("No team wins...");
-    }
-}
+const checkWinner = function (avgDolphins, avgKoalas) {
+  if (avgDolphins >= 2 * avgKoalas) {
+    console.log(`Dolphins win! (${avgDolphins} vs. ${avgKoalas})`);
+  } else if (avgKoalas >= 2 * avgDolphins) {
+    console.log(`Koalas win! (${avgKoalas} vs. ${avgDolphins})`);
+  } else {
+    console.log("No team wins...");
+  }
+};
 
 checkWinner(scoreDolphins, scoreKoalas);
 
 // Test 2
 scoreDolphins = calcAverage(85, 54, 41);
 scoreKoalas = calcAverage(23, 34, 27);
-console.log(scoreDolphins,scoreKoalas);
-checkWinner(scoreDolphins,scoreKoalas);;
+console.log(scoreDolphins, scoreKoalas);
+checkWinner(scoreDolphins, scoreKoalas);
