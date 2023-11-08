e30fdd8970240d6610562719c905672af203da78
osameissa <113057277+osameissa@users.noreply.github.com>
2023-11-08 15:51:21 +0100

    Challenge #7

--- challenges/challenge7.js
+++ challenges/challenge7.js
@@ -1,24 +1,28 @@
 const mark = {
-    fullName: 'Mark Miller',
-    mass: 78,
-    height: 1.69,
-    calcBMI: function () {
-        this.bmi = this.mass / this.height ** 2;
-        return this.bmi;
-    }
+  fullName: "Mark Miller",
+  mass: 78,
+  height: 1.69,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  },
 };
 
 const john = {
-    fullName: 'John Smith',
-    mass: 92,
-    height: 1.95,
-    calcBMI: function () {
-        this.bmi = this.mass / this.height ** 2;
-        return this.bmi;
-    }
-}
+  fullName: "John Smith",
+  mass: 92,
+  height: 1.95,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  },
+};
 
 mark.calcBMI();
 john.calcBMI();
 
-console.log(`${john.fullName}'s BMI (${john.bmi}) is ${john.bmi > mark.bmi ? 'higher' : 'lower'} than ${mark.fullName}'s (${mark.bmi})!`);
+console.log(
+  `${john.fullName}'s BMI (${john.bmi}) is ${
+    john.bmi > mark.bmi ? "higher" : "lower"
+  } than ${mark.fullName}'s (${mark.bmi})!`
+);
