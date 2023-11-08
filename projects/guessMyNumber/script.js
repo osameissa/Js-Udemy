7ffa75a0685f1a55af65b61e1651573db872ac64
osameissa <113057277+osameissa@users.noreply.github.com>
2023-11-09 00:05:24 +0100

    GuessMyNumber

---
+++ projects/guessMyNumber/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const number = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = number;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '😞 No number!';
+  }
+});
