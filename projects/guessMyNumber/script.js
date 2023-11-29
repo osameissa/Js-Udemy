643755a39e42192754df0b5be2e625f3a8f9a4ec
osameissa <113057277+osameissa@users.noreply.github.com>
2023-11-29 20:05:37 +0100

    Update script.js

--- projects/guessMyNumber/script.js
+++ projects/guessMyNumber/script.js
@@ -1,35 +1,51 @@
-// strict mode
 'use strict';
-
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
   if (!guess) {
-    document.querySelector('.message').textContent = '⛔ No Number!';
+    document.querySelector('.message').textContent = '🤷🏿‍♂️ No number';
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('.message').textContent =
+      '🍆💦 Yeees u winn belbol!';
     document.querySelector('.number').textContent = secretNumber;
-  } else if (guess > secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = '📈 Too high!';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = '😵 GAME OVER!';
-      document.querySelector('.score').textContent = 0;
+    document.querySelector('body').style.background = 'green';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
     }
   } else if (guess < secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = '📉 Too low!';
+      document.querySelector('.message').textContent = '📉 Belbol too low';
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = '😵 GAME OVER!';
+      document.querySelector('.message').textContent = '👾 GAME OVER BELBOL!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Belbol too high';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '👾 GAME OVER BELBOL!';
       document.querySelector('.score').textContent = 0;
     }
   }
 });
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  document.querySelector('body').style.background = 'black';
+  document.querySelector('.message').textContent = 'Start belboling...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.guess').value = '';
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.number').textContent = '?';
+});
+```
